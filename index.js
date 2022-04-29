c7734949fe9022c0e47e6db2c0b582b73aa5e0cb
arifraj199 <karif9514@gmail.com>
2022-04-30 02:23:10 +0600

    connect mongodb with server and get data from mongodb implement

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 const app = express();
 
@@ -10,10 +12,34 @@ app.use(cors());
 app.use(express.json());
 
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nfrv0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const fruitCollection = client.db("fruitsInventory").collection("fruit");
+
+        app.get('/inventory',async(req,res)=>{
+            const query = {};
+            const cursor = fruitCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    }
+    finally{}
+}
+
+run().catch(console.dir);
+
+
+
 app.get('/',(req,res)=>{
     res.send('fruits server is running')
 });
 
 app.listen(port,()=>{
-    console.log('db connected');
+    console.log('server is connected on port',port);
 })
