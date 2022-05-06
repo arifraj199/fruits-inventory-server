20578e2d8d42738ce1884295ff0030044452e0fb
arifraj199 <karif9514@gmail.com>
2022-05-06 21:06:54 +0600

    find error

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ function verifyJWT(req, res, next) {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env
   .DB_PASS}@cluster0.nfrv0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
