5f8e265dd3c6a6667b5f953ce99c1a341c1c939d
arifraj199 <karif9514@gmail.com>
2022-05-07 15:56:26 +0600

    jwt solving problem

--- index.js
+++ index.js
@@ -27,11 +27,12 @@ function verifyJWT(req, res, next) {
     if (err) {
       return res.status(403).send({ message: "Forbidden access" });
     }
-    // console.log('decoded',decoded);
+    console.log('decoded',decoded);
     req.decoded = decoded;
+    next();
   });
 
-  next();
+
 }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env
